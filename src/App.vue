920fba951290f0419cd07e320239f5a815f19cf8
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-07-20 17:29:59 -0300

    finalizando o menu movel (mobile)

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <template>
     <div>
-        <nav>
-            
-        </nav>
+        <NavbarContent :language="language" @change-to="changeLang = $event" />
         
         <router-view/>
     </div>
 </template>
 
+<script>
+import NavbarContent from '@/components/NavbarContent.vue'
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            language: [
+                { name: 'English', isCheck: true },
+                { name: 'Portuguese', isCheck: false }
+            ],
+            changeLang: ''
+        }
+    },
+    components: {
+        NavbarContent
+    },
+    watch: {
+        changeLang(toLang) {
+            console.log(toLang)
+            if(toLang == 'Portuguese') {
+                this.language[0].isCheck = false
+                this.language[1].isCheck = true
+            }else {
+                this.language[0].isCheck = true
+                this.language[1].isCheck = false
+            }
+        }
+    }
+}
+</script>
+
+
 <style lang="scss">
     // standard
     
-    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'); // mbuntu font
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'); // ubuntu font
     @import '@/assets/colors/colors.scss'; // colors
 
     .gradient { background-image: linear-gradient(to bottom right, $color-d-2, $color-l-2) } // main gradient
 
     a { &:hover { text-decoration: none; opacity: .7 } }; // link hover
     ul { list-style-type: none; } // list without style type
+    .btn, button, input { &:focus, &:hover { border-color: rgba(0,0,0,0); outline: none; } } // removing outline
 
     * { // space and font
         margin: 0;
@@ -26,5 +58,9 @@
     }
 
     html, body { width: 100%; height: 100%; }
+
+    #app {
+        padding: 1rem;
+    }
 
 </style>
---
+++ src/components/NavbarContent.vue
@@ -0,0 +1,137 @@
+<template>
+    <div>
+        <!-- open navbar button -->
+        <button class="btn bar" type="button" data-bs-toggle="offcanvas" data-bs-target="#menuMobile" aria-controls="menuMobile" ><font-awesome-icon icon="fa-solid fa-bars" /></button>
+
+        <div class="offcanvas offcanvas-start text-bg-dark" data-bs-scroll="true" tabindex="-1" id="menuMobile" aria-labelledby="menuMobileLabel">
+            <div class="offcanvas-header">
+                <!-- language -->
+                <div class="btn-group dropend">
+                    <button type="button" class="btn lang" data-bs-toggle="dropdown" aria-expanded="false">
+                        <font-awesome-icon icon="fa-solid fa-language" />
+                    </button>
+                    <ul class="dropdown-menu">
+                        <!-- Dropdown menu links -->
+                        <li 
+                            class="dropdown-item" 
+                            v-for="(lang, index) in language" 
+                            :key="index" 
+                            :class="{ disabled: lang.isCheck }"
+
+                            @click="changeTo(index)"
+                        >
+                            <button type="button">{{ lang.name }}</button>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- close navbar button -->
+                <button type="button" class="bar-close" data-bs-dismiss="offcanvas" aria-label="Close"><font-awesome-icon icon="fa-solid fa-bars" /></button>
+            </div>
+            <!-- navbar mobile content -->
+            <div class="offcanvas-body">
+                <!-- navbar mobile list -->
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a href="#" class="nav-link"><font-awesome-icon icon="fa-solid fa-house-chimney" /> Home</a></li>
+                    <li class="nav-item"><a href="#about" class="nav-link"><font-awesome-icon icon="fa-solid fa-book-open-reader" /> About</a></li>
+                    <li class="nav-item"><a href="#work" class="nav-link"><font-awesome-icon icon="fa-solid fa-code" /> Work</a></li>
+                    <li class="nav-item"><a href="#contact" class="nav-link"><font-awesome-icon icon="fa-solid fa-envelope" /> Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavbarContent',
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        language: Array
+    },
+    methods: {
+        changeTo(i) {
+            if(i == 0) this.$emit('changeTo', 'English')
+            else if(i == 1) this.$emit('changeTo', 'Portuguese')
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '@/assets/colors/colors.scss'; // colors
+
+    // all buttons
+    button {
+        padding: 0;
+        margin: 0;
+        background-color: rgba(0,0,0,0);
+        border: none;
+        font-size: 1.9em;
+        color: $color-l-1;
+
+        // bar button
+        &.bar {
+            color: $color-l-1;
+        }
+        // lang button
+        &.lang {
+            color: $color-l-3;
+        }
+    }
+
+    // change language button
+    .dropdown-menu {
+        background-color: rgba($color-l-3, 1);
+        .dropdown-item {
+            padding: 0;
+            button {
+                font-size: 1em;
+                color: $color-l-1;
+                width: 100%;
+                padding: 3px 0 3px 10px;
+                text-align: start;
+
+                &:hover, &:active {
+                    background-color: $color-l-1;
+                    color: $color-d-1;
+                }
+            }
+
+            &.disabled {
+                background-color: rgba($color-l-1, .3);
+            }
+        }
+    }
+
+    // navbar mobile width content
+    .offcanvas {
+        width: 20rem;
+    }
+    
+    @media only screen and (max-width: 576px) {
+        .offcanvas {
+            width: 15rem;
+        }
+    }
+    // navbar mobile list content
+    .nav {
+        .nav-item {
+            .nav-link {
+                padding: 5px 0;
+                margin: 5px 0;
+                color: $color-l-1;
+                text-decoration: underline;
+                text-decoration-color: $color-l-2;
+
+                svg {
+                    margin-right: 10px;
+                }
+            }
+        }
+    }
+</style>
