7788d2f78514a2fe8c501884faf2d4e0b690ccc0
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-07-20 13:32:57 -0300

    organizando projeto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+        <nav>
+            
+        </nav>
+        
+        <router-view/>
+    </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    // standard
+    
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,500;0,700;1,300;1,400;1,500;1,700&display=swap'); // mbuntu font
+    @import '@/assets/colors/colors.scss'; // colors
 
-nav {
-  padding: 30px;
+    .gradient { background-image: linear-gradient(to bottom right, $color-d-2, $color-l-2) } // main gradient
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a { &:hover { text-decoration: none; opacity: .7 } }; // link hover
+    ul { list-style-type: none; } // list without style type
 
-    &.router-link-exact-active {
-      color: #42b983;
+    * { // space and font
+        margin: 0;
+        padding: 0;
+        font-family: 'Ubuntu', 'Trebuchet MS', sans-serif;
     }
-  }
-}
+
+    html, body { width: 100%; height: 100%; }
+
 </style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="main">
+        <p>testing</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainView'
+}
+</script>
